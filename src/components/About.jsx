d825fb1a9cc072b0116a391e79c47ffc0f29b676
Niranjan Chaudhary <cdyniranjan7321@gmail.com>
2024-04-27 15:01:41 +0545

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { HiOutlineArrowRight } from "react-icons/hi";
+
+const About = () => {
+  return (
+    <>
+      <section className="about" id="about">
+        <div className="container">
+          <div className="banner">
+            <div className="top">
+              <h1 className="heading">ABOUT US</h1>
+              <p>The only thing we're serious about is food.</p>
+            </div>
+            <p className="mid">
+            We can provide the option to obtain the right suffering, because they pursue it, because they accuse the consequences of being misled, because there is no time to deny the mistake, because they assume the pain. Escape from the necessity of being greater, all of this being the exercise of the right to suffer, the right to pain, the right to accuse the consequences of this pain.
+            </p>
+            <Link to={"/"}>
+              Explore Menu{" "}
+              <span>
+                <HiOutlineArrowRight />
+              </span>
+            </Link>
+          </div>
+          <div className="banner">
+            <img src="about.png" alt="about" />
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default About;
